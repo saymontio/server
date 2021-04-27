74dedeb998fbba0ef84822b608e151dafa5d4a12
Marcos Vinicius <homemdemelo@gmail.com>
2021-04-27 00:47:02 -0300

    correção membros

--- pages/api/musicas.js
+++ pages/api/musicas.js
@@ -5,6 +5,32 @@ export default async function (request, response) {
     response.json({
         "episodes": [
             {
+                "id": "metalica-st-anger",
+                "title": "Metallica - St. Anger",
+                "members": "James Hetfield, Lars Ulrich, Kirk Hammett eRobert Trujillo",
+                "published_at": "2020-04-09 20:00:00",
+                "thumbnail": base_url + "music/metallica.jpg",
+                "description": "<p>Metallica é uma banda norte-americana de heavy metal originária de Los Angeles, mas com base em San Francisco. O seu repertório inclui tempos rápidos, pesados, melódicos, instrumentais e musicalidade agressiva, a qual os colocou no lugar de pioneiros do thrash metal e uma das bandas fundadoras do Big Four of Thrash, conjuntamente com Slayer, Megadeth e Anthrax. Os Metallica formaram-se em 1981, após James Hetfield responder a um anúncio que Lars Ulrich colocou no jornal local. A sua formação atual apresenta os fundadores Ulrich (bateria) e Hetfield (vocal e guitarra base), o guitarrista Kirk Hammett (que se juntou à banda em 1983), e o baixista Robert Trujillo (membro desde 2003). Antes de chegarem à sua formação atual, a banda teve outros integrantes, sendo eles: Dave Mustaine (guitarra), Ron McGovney, Cliff Burton e Jason Newsted (baixo). </p>",
+                "file": {
+                    "url": base_url + "music/Metallica_st_anger.webm",
+                    "type": "audio/x-m4a",
+                    "duration": 348
+                }
+            },
+            {
+                "id": "rhcp-dark-necessities",
+                "title": "RHCP - Dark Necessities",
+                "members": "Anthony Kiedis, Flea, Chad Smith e John Frusciante",
+                "published_at": "2020-04-09 20:00:00",
+                "thumbnail": base_url + "music/RHCP.png",
+                "description": "<p>Red Hot Chili Peppers (também conhecido como Red Hot ou RHCP) é uma banda de rock dos Estados Unidos formada em Los Angeles, Califórnia, em 13 de fevereiro de 1983, considerada uma das maiores bandas da história do rock. O estilo musical do grupo consiste principalmente no funk rock, bem como elementos de outros gêneros, tais como punk, rock alternativo, rap rock e rock psicodélico.[1] A banda é constituída pelos membros fundadores Anthony Kiedis (vocais) e Flea (baixo), juntamente com o baterista de longa data Chad Smith e o guitarrista John Frusciante.</p>",
+                "file": {
+                    "url": base_url + "music/RHCP_dark_necessities.webm",
+                    "type": "audio/x-m4a",
+                    "duration": 304
+                }
+            },
+            {
                 "id": "jinjer-pisces-live-session",
                 "title": "JINJER | Pisces (Live Session)",
                 "members": "Tatiana Shmailyuk, Roman Ibramkha, Max Fatullaev, Eugene Kostyuk e Vladislav Ulasevish",
@@ -18,22 +44,9 @@ export default async function (request, response) {
                 }
             },
             {
-                "id": "metalica-st-anger",
-                "title": "Metallica - St. Anger",
-                "members": "Diego Fernandes, Higo Ribeiro e Guilherme Pellizzetti",
-                "published_at": "2020-04-09 20:00:00",
-                "thumbnail": base_url + "music/metallica.jpg",
-                "description": "<p>Metallica é uma banda norte-americana de heavy metal originária de Los Angeles, mas com base em San Francisco. O seu repertório inclui tempos rápidos, pesados, melódicos, instrumentais e musicalidade agressiva, a qual os colocou no lugar de pioneiros do thrash metal e uma das bandas fundadoras do Big Four of Thrash, conjuntamente com Slayer, Megadeth e Anthrax. Os Metallica formaram-se em 1981, após James Hetfield responder a um anúncio que Lars Ulrich colocou no jornal local. A sua formação atual apresenta os fundadores Ulrich (bateria) e Hetfield (vocal e guitarra base), o guitarrista Kirk Hammett (que se juntou à banda em 1983), e o baixista Robert Trujillo (membro desde 2003). Antes de chegarem à sua formação atual, a banda teve outros integrantes, sendo eles: Dave Mustaine (guitarra), Ron McGovney, Cliff Burton e Jason Newsted (baixo). </p>",
-                "file": {
-                    "url": base_url + "music/Metallica_st_anger.webm",
-                    "type": "audio/x-m4a",
-                    "duration": 348
-                }
-            },
-            {
                 "id": "metalica-mama-said",
                 "title": "Metallica - Mama Said",
-                "members": "Diego Fernandes, Higo Ribeiro e Guilherme Pellizzetti",
+                "members": "James Hetfield, Lars Ulrich, Kirk Hammett eRobert Trujillo",
                 "published_at": "2020-04-09 20:00:00",
                 "thumbnail": base_url + "music/metallica.jpg",
                 "description": "<p>Metallica é uma banda norte-americana de heavy metal originária de Los Angeles, mas com base em San Francisco. O seu repertório inclui tempos rápidos, pesados, melódicos, instrumentais e musicalidade agressiva, a qual os colocou no lugar de pioneiros do thrash metal e uma das bandas fundadoras do Big Four of Thrash, conjuntamente com Slayer, Megadeth e Anthrax. Os Metallica formaram-se em 1981, após James Hetfield responder a um anúncio que Lars Ulrich colocou no jornal local. A sua formação atual apresenta os fundadores Ulrich (bateria) e Hetfield (vocal e guitarra base), o guitarrista Kirk Hammett (que se juntou à banda em 1983), e o baixista Robert Trujillo (membro desde 2003). Antes de chegarem à sua formação atual, a banda teve outros integrantes, sendo eles: Dave Mustaine (guitarra), Ron McGovney, Cliff Burton e Jason Newsted (baixo). </p>",
@@ -41,19 +54,6 @@ export default async function (request, response) {
                     "url": base_url + "music/Metallica_mama_said.webm",
                     "type": "audio/x-m4a",
                     "duration": 295
-                }
-            },
-            {
-                "id": "rhcp-dark-necessities",
-                "title": "RHCP - Dark Necessities",
-                "members": " Anthony Kiedis, Flea, Chad Smith e John Frusciante",
-                "published_at": "2020-04-09 20:00:00",
-                "thumbnail": base_url + "music/RHCP.png",
-                "description": "<p>Red Hot Chili Peppers (também conhecido como Red Hot ou RHCP) é uma banda de rock dos Estados Unidos formada em Los Angeles, Califórnia, em 13 de fevereiro de 1983, considerada uma das maiores bandas da história do rock. O estilo musical do grupo consiste principalmente no funk rock, bem como elementos de outros gêneros, tais como punk, rock alternativo, rap rock e rock psicodélico.[1] A banda é constituída pelos membros fundadores Anthony Kiedis (vocais) e Flea (baixo), juntamente com o baterista de longa data Chad Smith e o guitarrista John Frusciante.</p>",
-                "file": {
-                    "url": base_url + "music/RHCP_dark_necessities.webm",
-                    "type": "audio/x-m4a",
-                    "duration": 304
                 }
             }
         ]
